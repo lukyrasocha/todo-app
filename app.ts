c65371360143221e7b7373e42dc9eb61426dcdca
lukyrasocha <rasocha.lukas@gmail.com>
2022-02-01 23:30:45 +0100

    added a working connection to a mysql server

--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
 const express = require( "express" );
 const bodyParser = require('body-parser');
 const app = express();
+const mysql = require('mysql');
+require('dotenv').config();
 
 const port = 5000; // default port to listen
 import router from "./routers/todos.router"
@@ -8,6 +10,32 @@ import router from "./routers/todos.router"
 app.get( "/api", function ( req, res ) {
     res.send( "Hello world!" );
 });
+
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+})
+
+connection.connect((err) => {
+    if(!err) {
+        console.log('Connection to MYSQL successful, fuck yes')
+    } else{
+        console.log('Connection failed:',err)
+    }
+
+})
+
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+    if (err) throw err
+
+    console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
+
 
 
 // parse application/x-www-form-urlencoded
