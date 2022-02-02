f99747afb747b40adaa07bac7de7e2bd04bbcecc
lukyrasocha <rasocha.lukas@gmail.com>
2022-02-02 16:49:37 +0100

    added more data into my database

--- db_schema/populate.sql
+++ db_schema/populate.sql
@@ -15,3 +15,9 @@ INSERT INTO User(email,name) VALUES('lukas@gmail.com','Luky Rasocha');
 INSERT INTO User(email,name) VALUES('robert@gmail.com','Robert Spralja');
 INSERT INTO User(email,name) VALUES('ludek@gmail.com','Ludek Cizinsky');
 INSERT INTO User(email,name) VALUES('mika@gmail.com','Mika Senghaas');
+
+-- TODOS
+INSERT INTO Todo(id,status,category,assignee,assigned,title,description,date_added,date_completed) VALUES('2f3as3fe4442s',1,1,'lukas@gmail.com','michael@gmail.com','First assignment','This is my first assignment for computas, hope it goes well',sysdate(),sysdate());
+INSERT INTO Todo(id,status,category,assignee,assigned,title,description,date_added,date_completed) VALUES('3fass325kasls',2,2,'robert@gmail.com','mika@gmail.com','Do this for me','This the second assignment for computas, hope it goes well',sysdate(),sysdate());
+
+select * from todo
