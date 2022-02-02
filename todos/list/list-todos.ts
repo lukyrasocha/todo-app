81edd2888755b89ee8cd5738b01c4a5f4d27efc0
lukyrasocha <rasocha.lukas@gmail.com>
2022-02-02 16:47:07 +0100

    added an endpoint for listing all the todos from mysql database

--- todos/list/list-todos.ts
+++ todos/list/list-todos.ts
@@ -1,5 +1,30 @@
 import {data} from "../../data";
 import {ITodo} from "../../interfaces/ITodo";
+import connection from '../../connection';
+import {IUser} from "../../interfaces/IUser";
+import {ECategories} from "../../enums/Ecategories";
+import {EStatuses} from "../../enums/Estatus";
+
 export async function listTodos(): Promise<ITodo[]>{
-    return Promise.resolve(data);
+    const result:any = await connection.query('SELECT * FROM todo');
+
+    const todos:ITodo[] = [];
+
+    for(const row of result){
+        const todo:ITodo = {
+            id: row.id,
+            title: row.title,
+            assignee: row.assignee,
+            assigned: row.assigned,
+            category: row.category,
+            status: row.status,
+            description: row.description,
+            dateAdded: row.date_added,
+            dateCompleted: row.date_completed
+        }
+
+        todos.push(todo)
+    }
+
+    return Promise.resolve(todos);
 }
