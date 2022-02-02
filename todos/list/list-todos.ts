74a1c7ca30dbe3f94454f431ba73f5698e1685d2
lukyrasocha <rasocha.lukas@gmail.com>
2022-02-02 16:50:10 +0100

    added a working endpoint for listing all todos from my db

--- todos/list/list-todos.ts
+++ todos/list/list-todos.ts
@@ -1,9 +1,6 @@
-import {data} from "../../data";
 import {ITodo} from "../../interfaces/ITodo";
 import connection from '../../connection';
-import {IUser} from "../../interfaces/IUser";
-import {ECategories} from "../../enums/Ecategories";
-import {EStatuses} from "../../enums/Estatus";
+
 
 export async function listTodos(): Promise<ITodo[]>{
     const result:any = await connection.query('SELECT * FROM todo');
