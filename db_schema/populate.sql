2ef2cf37e861c4d56274b4afaacf3a4b2bf725f8
lukyrasocha <rasocha.lukas@gmail.com>
2022-02-02 14:41:04 +0100

    SQL script to recreate tables for the db defined in the assignment + populating the db

---
+++ db_schema/populate.sql
@@ -0,0 +1,17 @@
+-- CATEGORIES
+INSERT INTO Category(category) VALUES(1);
+INSERT INTO Category(category) VALUES(2);
+INSERT INTO Category(category) VALUES(3);
+
+-- STATUS
+INSERT INTO Status(status) VALUES('Assigned');
+INSERT INTO Status(status) VALUES('In progress');
+INSERT INTO Status(status) VALUES('Stuck');
+INSERT INTO Status(status) VALUES('Done');
+
+-- USERS
+INSERT INTO User(email,name) VALUES('michael@gmail.com','Michael Marigliano');
+INSERT INTO User(email,name) VALUES('lukas@gmail.com','Luky Rasocha');
+INSERT INTO User(email,name) VALUES('robert@gmail.com','Robert Spralja');
+INSERT INTO User(email,name) VALUES('ludek@gmail.com','Ludek Cizinsky');
+INSERT INTO User(email,name) VALUES('mika@gmail.com','Mika Senghaas');
