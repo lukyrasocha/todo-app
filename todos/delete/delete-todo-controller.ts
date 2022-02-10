a4dcce8898fe9fc5a75fe3a95372babdf31dd9be
lukyrasocha <rasocha.lukas@gmail.com>
2022-02-10 23:49:16 +0100

    added a type for one variable

--- todos/delete/delete-todo-controller.ts
+++ todos/delete/delete-todo-controller.ts
@@ -3,7 +3,7 @@ import { ITodo } from "../../interfaces/ITodo";
 import { deleteTodo } from "./delete-todo";
 
 export async function deleteTodoController(req: Request, res: Response) {
-  const deletedID = await deleteTodo(req.params.id);
+  const deletedID: String = await deleteTodo(req.params.id);
 
   return res.status(200).json(`Deleted todo with id ${deletedID}`);
 }
