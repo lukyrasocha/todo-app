d5b2ab1afa7af0de80132acf918d58ca2af16e32
Lukas Rasocha <rasocha.lukas@gmail.com>
2022-02-24 05:25:09 -0800

    added a route for api/users

--- routers/todos.router.ts
+++ routers/todos.router.ts
@@ -4,11 +4,12 @@ import {getTodoController} from "../todos/get/get-todo-controller";
 import {createTodoController} from "../todos/create/create-todo-controller";
 import {updateTodoController} from "../todos/update/update-todo-controller";
 import {deleteTodoController} from "../todos/delete/delete-todo-controller";
-
+import { getUsersController } from "../users/getUsers/get-users-controller"; 
 const router = express.Router();
 
 router.get('/todos', listTodosController);
 router.get('/todos/:id', getTodoController);
+router.get('/users', getUsersController);
 router.post('/todos', createTodoController);
 router.put('/todos', updateTodoController);
 router.delete('/todos/:id', deleteTodoController);
